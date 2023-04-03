f7801840ead0a4b1205a75c1d41ab58c2e459ce3
Vinicius Ferreira <viniciusfe66@gmail.com>
2023-04-03 07:56:33 -0400

    primeiro commit

---
+++ enviar.php
@@ -0,0 +1,5 @@
+<?php
+$mensagem = utf8_decode('Nome: '.$_POST['name']. "\r\n" .'Telefone: '.$_POST['number']."\r\n"."\r\n". 'Mensagem: '.$_POST['message']);
+$cabecalhos ="From: ".$_POST['email'];
+@mail("faconsultoriasegtrab@gmail.com", $_POST["subject"], $menssagem, $cabecalhos);
+header("Location: index.php");
---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="ManoBlack.dev">
+    <meta name="keywords" content="Segurança, trabalho, cuiaba, fa , fernado almeida, mato grosso, brigada de incendio, nr, alturas, especialista, site, segurança do trabalho">
+    <meta name="description" content="Temos varios cursos de segurança do0 trabalho, e certificações">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <title>FA CONSULTORIA GESTÃO TREINAMENTOS SEGURANÇA TRABALHO</title>
+
+    <!-- Fav Icons -->
+    <link rel="shortcut icon" href="assets/images/logo-faa.png" type="image/x-icon">
+
+    <!-- Stylesheets -->
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-v5.9.0.min.css">
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/leaflet.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/spacing.min.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <div class="page-wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader"></div>
+
+        <!--======= Start Header area =======-->
+        <header class="main-header">
+
+            <!--Header-Upper-->
+            <div class="header-upper">
+                <div class="container clearfix">
+
+                    <div class="header-inner d-lg-flex align-items-center">
+
+                        <div class="logo-outer d-flex align-items-center">
+                            <div class="logo"><a href="index.html"><img src="assets/images/logo-faa.png" alt="" title=""></a></div>
+                        </div>
+
+                        <div class="nav-outer clearfix ml-lg-auto">
+                            <!-- Main Menu -->
+                            <nav class="main-menu navbar-expand-lg">
+                                <div class="navbar-header clearfix">
+                                    <!-- Toggle Button -->
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+
+                                <div class="navbar-collapse collapse clearfix">
+                                    <ul class="navigation clearfix">
+                                        <li class="current-menu-item"><a href="index.html">Inicio</a></li>
+                                        <li><a href="#sobre">Sobre</a></li>
+                                        <li><a href="#servico">Serviços</a></li>
+                                        <li><a href="#contato">Contate-nós</a></li>
+                                    </ul>
+                                </div>
+
+                            </nav>
+                            <!-- Main Menu End-->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!--End Header Upper-->
+        </header>
+        <!--======== End Header area ========-->
+
+       
+        <!--====== Start Hero Section =======-->
+        <section class="hero-section overlay">
+            <div class="container">
+                <div class="hero-inner">
+                    <h1 class="wow fadeInUp" data-wow-duration="1s">Consultoria gestão e treinamento segurança do trabalho</h1>
+                    
+                </div>
+            </div>
+        </section>
+        <!--========  End Hero Section ========-->
+        
+        
+        <!--===== Start Featured Section ======-->
+        <section class="featured-section pt-115 pb-85">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="featured-box">
+                            <i class="fas fa-file"></i>
+                            <h3><a href="service-details.html">E-Social <br> realizando todos eventos</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="featured-box">
+                            <i class="fas fa-file-alt"></i>
+                            <h3><a href="service-details.html">Emissão de PGR, PCMSO, PGRTR, LTCAT, PRONTUÁRIO NR20 E LAUDOS</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="featured-box">
+                            <i class="fas fa-graduation-cap"></i>
+                            <h3><a href="service-details.html">Todos TREINAMENTOS Das NR'S</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--===== End Featured Section ========-->
+        
+        
+        <!--======= Start About Section ========-->
+        <section class="about-section relative" id="sobre">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="about-image"></div>
+                    </div>
+                    <div class="col-xl-6">
+                        <div class="about-content pb-80">
+                            <div class="section-title mb-20">
+                                <span>Sobré a empresa</span>
+                                <h2>Vamos saber um pouco mais sobre nossa empresa ?</h2>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p>
+                                        A FA presta serviços de consultoria e treinamentos em Segurança do trabalho em Cuiabá e também em todo Estado de Mato Grosso e Centro Oeste. Atende com seriedade e competência realizando também serviços nas áreas de treinamentos e cursos, elaboração de laudos diversos e assistência técnica em perícias envolvendo processos trabalhistas para levantamento de insalubridade e periculosidade.
+                                        <br>
+                                        A FA atende com qualidade e excelência em Segurança do Trabalho, realizando um serviço totalmente personalizado e alinhado às demandas e particularidades de cada cliente.
+                                        <br>
+                                        A principal missão é oferecer soluções de segurança do trabalho através de elaboração de programas, palestras, treinamentos, assessoria e consultoria para empresas de todos os segmentos.</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="about-number-side mb-35">
+                                        <i class="fab fa-whatsapp"></i>
+                                        <div class="number-right">
+                                            <h4>Faça um Orçamento</h4>
+                                            <a href="https://wa.me/message/U7EF7I2VXSQVC1" target="_blank">+55 (65) 99268-9353</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="about-shape">
+            </div>
+        </section>
+        <!--======== End About Section =========-->
+
+       
+        <!--===== Start Services Section =======-->
+        <section class="services-section relative pt-240 pb-115" id="servico">
+            <div class="service-overlay overlay">
+                
+            </div>
+            <div class="container">
+                <div class="section-title text-white text-center mb-70">
+                    <span>Nossos serviços</span>
+                    <h2>Serviço que oferecemos</h2>
+                </div>
+                <div class="row no-gap justify-content-center">
+                    <div class="col-xl-4 col-md-6">
+                        <div class="service-box">
+                           <img src="assets/images/nr20.jpg" alt="Service">
+                            <div class="service-content">
+                                <i class="fas fa-fire-extinguisher"></i>
+                                <h3><a href="service-details.html">Brigada de incêndio</a></h3>
+                                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="service-box">
+                           <img src="assets/images/nr35.jpg" alt="Service">
+                            <div class="service-content">
+                                <i class="fas fa-cloud"></i>
+                                <h3><a href="service-details.html">Trabalho em alturas</a></h3>
+                                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="service-box mb-0">
+                           <img src="assets/images/nr9.jpg" alt="Service">
+                            <div class="service-content">
+                                <i class="fas fa-gas-pump"></i>
+                                <h3><a href="service-details.html">Exposição ao benzeno</a></h3>
+                                <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End Services Section =======-->
+
+        <!--========== Start Contact Page ============-->
+        <section class="contact-page pt-110 pb-115 rpb-95" id="contato">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="contact-info rmb-55">
+                            <h2>Informação para contato</h2>
+                            <div class="info-item">
+                                <div class="info-icon">
+                                    <i class="flaticon-mail"></i>
+                                </div>
+                                <div class="info-content">
+                                    <h3>Endereço de Email</h3>
+                                    <p><a href="mailto:faconsultoriasegtrab@gmail.com">faconsultoriasegtrab@gmail.com</a></p>
+                                    <!-- <p><a href="mailto:yourbord@edu.com">yourbord@edu.com</a></p> -->
+                                </div>
+                            </div>
+                            <div class="info-item">
+                                <div class="info-icon">
+                                    <i class="flaticon-phone-call"></i>
+                                </div>
+                                <div class="info-content">
+                                    <h3>numero de telefone</h3>
+                                    <p><a href="callto:5565992689353">+55 (65) 99268-9353</a></p>
+                                    <!-- <p><a href="callto:12345678901">+1 (234) 567 8901</a></p> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="contact-form">
+                            <h2>Faça seu Orçamento</h2>
+                            <form action="enviar.php">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="name" name="name" value="" placeholder="Seu Nome" required="">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="email" class="form-control" id="email" name="email" value="" placeholder="Seu Email" required="">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="subject" name="subject" value="" placeholder="Assunto">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="number" name="number" value="" placeholder="Numero de Telefone">
+                                    </div>
+                                    <div class="col-12">
+                                        <textarea class="form-control" id="message:" name="message:" rows="6" placeholder="Menssagem" required=""></textarea>
+                                    </div>
+                                    <div class="col-12">
+                                        <button type="submit" class="theme-btn bg-yellow">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--=========== End Contact Page ==============-->
+        
+        <!-- Scroll Top Button -->
+        <button class="scroll-top scroll-to-target" data-target="html"><i class="fas fa-angle-double-up"></i></button>
+        
+    </div>
+    <!--End pagewrapper-->
+    
+    <!-- jequery plugins -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+    <!--[if lt IE 9]><script src="assets/js/html5shiv-3.7.3.js"></script><![endif]-->
+
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/leaflet.min.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <!-- Custom script -->
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
